2323e0537a0a76571dc797cd192fabf4b7f99d4a
Bridget <bridgetoconnor808@gmail.com>
2020-09-17 14:43:44 -1000

    working on fixing eslint

--- charitable/.eslintrc.js
+++ charitable/.eslintrc.js
@@ -4,24 +4,24 @@ module.exports = {
         ecmaVersion: 2020, // Use the latest ecmascript standard
         sourceType: 'module', // Allows using import/export statements
         ecmaFeatures: {
-            jsx: true // Enable JSX since we're using React
-        }
+            jsx: true, // Enable JSX since we're using React
+        },
     },
     settings: {
         react: {
-            version: 'detect' // Automatically detect the react version
-        }
+            version: 'detect', // Automatically detect the react version
+        },
     },
     env: {
         browser: true, // Enables browser globals like window and document
         amd: true, // Enables require() and define() as global variables as per the amd spec.
-        node: true // Enables Node.js global variables and Node.js scoping.
+        node: true, // Enables Node.js global variables and Node.js scoping.
     },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
+        'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
     ],
     rules: {
         'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
@@ -31,8 +31,8 @@ module.exports = {
             {
                 components: ['Link'],
                 specialLink: ['hrefLeft', 'hrefRight'],
-                aspects: ['invalidHref', 'preferButton']
-            }
-        ]
-    }
+                aspects: ['invalidHref', 'preferButton'],
+            },
+        ],
+    },
 }
