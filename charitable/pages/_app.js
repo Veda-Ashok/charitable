5ce4e4431dc9bcc414f1d0010566d27f74fc7912
Bridget <bridgetoconnor808@gmail.com>
2020-09-17 15:28:00 -1000

    disable prop type

--- charitable/pages/_app.js
+++ charitable/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp
