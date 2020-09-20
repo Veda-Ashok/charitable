6f6b77c84718e03cbbeaf88151123386ed0deae7
Bridget <bridgetoconnor808@gmail.com>
2020-09-19 23:34:39 -1000

    adding jest react testing library and husky

--- charitable_app/pages/_app.js
+++ charitable_app/pages/_app.js
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types'
 
-// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object,
 }
 
 export default MyApp
