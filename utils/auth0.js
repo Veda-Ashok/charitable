9b217be8bfca977b5b538f732767400a9b60f3bc
Bridget <bridgetoconnor808@gmail.com>
2020-09-25 00:04:11 -1000

    extreme basic login and logout working with auth0

--- utils/auth0.js
+++ utils/auth0.js
@@ -13,8 +13,6 @@ export default initAuth0({
     // The cookie lifetime (expiration) in seconds. Set to 8 hours by default.
     cookieLifetime: 60 * 60 * 8,
     // (Optional) The cookie domain this should run on. Leave it blank to restrict it to your domain.
-    cookieDomain: 'your-domain.com',
-    // (Optional) SameSite configuration for the session cookie. Defaults to 'lax', but can be changed to 'strict' or 'none'. Set it to false if you want to disable the SameSite setting.
     cookieSameSite: 'lax',
     // (Optional) Store the id_token in the session. Defaults to false.
     storeIdToken: false,
@@ -22,11 +20,5 @@ export default initAuth0({
     storeAccessToken: false,
     // (Optional) Store the refresh_token in the session. Defaults to false.
     storeRefreshToken: false,
-  },
-  oidcClient: {
-    // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
-    httpTimeout: 2500,
-    // (Optional) Configure the clock tolerance in milliseconds, if the time on your server is running behind.
-    clockTolerance: 10000,
   },
 })
