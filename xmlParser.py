6d4d883db92b665177e2fb4818f0f3df17e36f4f
Veda Ashok <veda@Nagashrees-MacBook-Pro.local>
2020-10-09 12:57:10 -0700

    fixed accessing attributes in arrays. added url attribute

--- xmlParser.py
+++ xmlParser.py
@@ -9,16 +9,28 @@ def getAttributeText(n, s):
         attributetext = 'Not Specified.' if checkattribute is None else checkattribute.text
         return attributetext
 
+def getAllElements(m, s1, s2):
+     results = []
+     for s1 in m.iter(s1):
+        for x in s1.findall(s2):
+            attribute = getAttributeText(x, 'name')
+            results.append(attribute)
+     return results
+
 for i in root.findall('organization'):
     name = getAttributeText(i, 'name')
     mission = getAttributeText(i, 'mission')
-    logoUrl = getAttributeText(i, 'logoUrl')
-    country = getAttributeText(i, 'country')
+    logo_url = getAttributeText(i, 'logoUrl')
+    url = getAttributeText(i, 'url')
+    countries = getAttributeText(i, 'countries')
+    all_countries = getAllElements(i, 'countries', 'country')
     themes = getAttributeText(i, 'themes')
+    all_themes = getAllElements(i, 'themes', 'theme')
+
    
     print('name: ', name, '\n', 'mission:', mission, '\n', 
-    'logoUrl: ', logoUrl, '\n', 'country: ', country, '\n',
-    'themes: ', themes, '\n', '\n')
+    'logo_url: ', logo_url, '\n', 'url: ', url, '\n', 'countries: ', all_countries, '\n',
+    'themes: ', all_themes, '\n', '\n')
 
 
     
