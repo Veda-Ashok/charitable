25f79bfd754290d75302dacdd941d03d67fc125e
sgibson1682 <samgibson1682@gmail.com>
2020-11-13 12:52:46 -0800

    fixed tests

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import auth0 from '../utils/auth0'
 import PropTypes from 'prop-types'
 import TrendingPage from '../src/components/TrendingPage'
-import { connectToDatabase } from '../utils/mongodb'
+// import { connectToDatabase } from '../utils/mongodb'
 
-export default function Trending({ user, charit_user }) {
+export default function Trending({ user }) {
   return (
     <div>
-      <h1>{charit_user[0].name}</h1>
       <TrendingPage user={user} />
     </div>
   )
@@ -14,18 +13,18 @@ export default function Trending({ user, charit_user }) {
 
 export async function getServerSideProps({ req }) {
   const session = await auth0.getSession(req)
-  const { db } = await connectToDatabase()
-  const users = await db.collection('users').find({}).limit(1).toArray()
+  // const { db } = await connectToDatabase()
+  // const users = await db.collection('users').find({}).limit(1).toArray()
 
   return {
     props: {
       user: session?.user || null,
-      charit_user: JSON.parse(JSON.stringify(users)),
+      // charit_user: JSON.parse(JSON.stringify(users)),
     },
   }
 }
 
 Trending.propTypes = {
   user: PropTypes.object,
-  charit_user: PropTypes.array,
+  // charit_user: PropTypes.array,
 }
