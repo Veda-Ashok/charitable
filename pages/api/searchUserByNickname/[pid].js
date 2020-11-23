8e643dfe862efbf6abef86e5621d6f3a87cad4d9
Veda Ashok <vashok061@gmail.com>
2020-11-22 22:18:43 -0800

    Trying to findOne then aggregate.

--- pages/api/searchUserByNickname/[pid].js
+++ pages/api/searchUserByNickname/[pid].js
@@ -8,13 +8,36 @@ export default async (req, res) => {
 
   const { db } = await connectToDatabase()
 
-  const users = await db.collection('users').findOne(
-    { nickname: `${pid.replace(/['"]+/g, '')}` },
-    {
-      _id: 1,
-      email: 0,
-    }
-  )
-
+  const users = await db
+    .collection('users')
+    .findOne(
+      { nickname: `${pid.replace(/['"]+/g, '')}` },
+      {
+        _id: 1,
+        email: 0,
+      }
+    )
+    .aggregate([
+      {
+        $lookup: {
+          from: 'organizations',
+          localField: 'saved_orgs',
+          foreignField: 'gg_id',
+          as: 'saved_orgs_docs',
+        },
+      },
+    ])
   res.json(users)
 }
+
+// db.orders.aggregate([
+//   {
+//     $lookup:
+//       {
+//         from: "inventory",
+//         localField: "item",
+//         foreignField: "sku",
+//         as: "inventory_docs"
+//       }
+//  }
+// ])
