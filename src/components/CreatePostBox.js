16b4aad6b75edcb1fad35490fbece072f058599c
Veda Ashok <vashok061@gmail.com>
2020-11-23 17:32:14 -0800

    Need to fix handleSuccessOpen if statement.

--- src/components/CreatePostBox.js
+++ src/components/CreatePostBox.js
@@ -13,6 +13,7 @@ import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import InfoSmallBox from './InfoSmallBox'
+import axios from 'axios'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -91,8 +92,33 @@ export default function CreatePostBox({
       handleOpen()
     } else {
       setInput('')
+
+      const createPost = async () => {
+        try {
+          const organization_id =
+            type === 'trending'
+              ? result.organization.id.toString()
+              : type === 'organizations'
+              ? result.gg_id
+              : undefined
+          const activity_id = type === 'activities' ? result._id : undefined
+          let response = await axios.post('/api/createPost', {
+            poster: dbuser._id,
+            image: undefined,
+            organization_id: organization_id,
+            activity_id: activity_id,
+            typed_content: input,
+          })
+          //whatever it shows for a successful insert check for that
+          if (response.data.matchedCount === 1 && response.data.modifiedCount === 1) {
+            handleSuccessOpen()
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      }
+      createPost()
       //send the post data to the post api
-      handleSuccessOpen()
       if (result && type) {
         closePostDialog()
       }
