4eb28b528156b1f68eec911032f4780160e268b8
sgibson1682 <samgibson1682@gmail.com>
2020-11-23 20:41:53 -0800

    MONGODB FUCKING LIED TO ME

--- pages/api/searchUserByNickname/[pid].js
+++ pages/api/searchUserByNickname/[pid].js
@@ -1,5 +1,11 @@
 import { connectToDatabase } from '../../../utils/mongodb'
 // const ObjectId = require('mongodb').ObjectID
+const arrayFromCursor = async (cursor) => {
+  //  cursor.forEach is asynchronous!
+  const result = []
+  await cursor.forEach((item) => result.push(item))
+  return result
+}
 
 export default async (req, res) => {
   const {
@@ -8,26 +14,25 @@ export default async (req, res) => {
 
   const { db } = await connectToDatabase()
 
-  const users = await db
-    .collection('users')
-    .findOne(
-      { nickname: `${pid.replace(/['"]+/g, '')}` },
-      {
-        _id: 1,
-        email: 0,
-      }
-    )
-    .aggregate([
-      {
-        $lookup: {
-          from: 'organizations',
-          localField: 'saved_orgs',
-          foreignField: 'gg_id',
-          as: 'saved_orgs_docs',
-        },
+  const agg = [
+    {
+      $match: {
+        nickname: `${pid.replace(/['"]+/g, '')}`,
       },
-    ])
-  res.json(users)
+    },
+    {
+      $lookup: {
+        from: 'organizations',
+        localField: 'saved_orgs',
+        foreignField: 'gg_id',
+        as: 'saved_orgs_docs',
+      },
+    },
+  ]
+
+  const users = await db.collection('users').aggregate(agg)
+  const result = await arrayFromCursor(users)
+  res.json(result[0])
 }
 
 // db.orders.aggregate([
@@ -41,3 +46,36 @@ export default async (req, res) => {
 //       }
 //  }
 // ])
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+// const MongoClient = require('mongodb').MongoClient
+// const assert = require('assert')
+// const agg = [
+//   {
+//     $match: {
+//       nickname: 'rachel',
+//     },
+//   },
+//   {
+//     $lookup: {
+//       from: 'organizations',
+//       localField: 'saved_orgs',
+//       foreignField: 'gg_id',
+//       as: 'saved_orgs_docs',
+//     },
+//   },
+// ]
+
+// MongoClient.connect('', { useNewUrlParser: true, useUnifiedTopology: true }, function (
+//   connectErr,
+//   client
+// ) {
+//   const coll = client.db('').collection('')
+//   coll.aggregate(agg, (cmdErr, result) => {
+//     assert.equal(null, cmdErr)
+//   })
+//   client.close()
+// })
