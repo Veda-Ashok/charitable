ac173fe16386754e9799b2bc54ae167e81a7ff39
sgibson1682 <samgibson1682@gmail.com>
2020-11-23 20:43:30 -0800

    bless dondi our savior, but still fuck mongo that was r00d

--- pages/api/searchUserByNickname/[pid].js
+++ pages/api/searchUserByNickname/[pid].js
@@ -1,5 +1,5 @@
 import { connectToDatabase } from '../../../utils/mongodb'
-// const ObjectId = require('mongodb').ObjectID
+
 const arrayFromCursor = async (cursor) => {
   //  cursor.forEach is asynchronous!
   const result = []
@@ -34,48 +34,3 @@ export default async (req, res) => {
   const result = await arrayFromCursor(users)
   res.json(result[0])
 }
-
-// db.orders.aggregate([
-//   {
-//     $lookup:
-//       {
-//         from: "inventory",
-//         localField: "item",
-//         foreignField: "sku",
-//         as: "inventory_docs"
-//       }
-//  }
-// ])
-
-/*
- * Requires the MongoDB Node.js Driver
- * https://mongodb.github.io/node-mongodb-native
- */
-// const MongoClient = require('mongodb').MongoClient
-// const assert = require('assert')
-// const agg = [
-//   {
-//     $match: {
-//       nickname: 'rachel',
-//     },
-//   },
-//   {
-//     $lookup: {
-//       from: 'organizations',
-//       localField: 'saved_orgs',
-//       foreignField: 'gg_id',
-//       as: 'saved_orgs_docs',
-//     },
-//   },
-// ]
-
-// MongoClient.connect('', { useNewUrlParser: true, useUnifiedTopology: true }, function (
-//   connectErr,
-//   client
-// ) {
-//   const coll = client.db('').collection('')
-//   coll.aggregate(agg, (cmdErr, result) => {
-//     assert.equal(null, cmdErr)
-//   })
-//   client.close()
-// })
