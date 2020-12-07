e007e213a13a2a078e24f1ff94c171dd54de4383
Bridget <bridgetoconnor808@gmail.com>
2020-12-07 11:39:10 -1000

    trying to fix deploy pt 2

--- pages/api/organizations.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Organization } from '../../utils/database'
-
-export default async (req, res) => {
-  const org = await Organization.findOne()
-  res.statusCode = 200
-  res.json({ name: org.get('name'), description: org.get('description') })
-}
